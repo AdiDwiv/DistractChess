9f53d3f55fd9772def97538e071cba657200f169
Aditya Dwivedi <Adi@dhcp-clara-2611.eduroam.cornell.edu>
2016-11-24 14:51:36 -0500

    Initial Commit

---
+++ ChessNGunsV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChessNGunsV2
+//
+//  Created by Aditya Dwivedi on 11/24/16.
+//  Copyright © 2016 org.cuappdev.project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
